f0d90d15ab411d071af41ca965d897716a881b62
Mike Oles <mbo10@pitt.edu>
2018-05-24 11:26:31 -0400

    add search

--- MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -11,6 +11,11 @@ namespace MvcMovie {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Movies",
+                url: "{controller}/{action}/{id}"
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
